e3fbbb8338cf08970f6144a8eb2083f9e62a5815
noxayyyy <aisuru.nox@gmail.com>
2026-03-08 17:28:02 +0500

    AST implementation

--- PA2/src/ast.cpp
+++ PA2/src/ast.cpp
@@ -1,56 +1,114 @@
-#include <iostream>     
-#include <cstring>
 #include "ast.h"
+#include <cstring>
+#include <iostream>
 
 using namespace std;
 
-ASTNode* create_node(NodeType type, char* data, int line, ASTNode* left, ASTNode* right, ASTNode* extra)
-{
+ASTNode*
+create_node(NodeType type, char* data, int line, ASTNode* left, ASTNode* right, ASTNode* extra) {
+	char* data_cpy = nullptr;
+	if (data) {
+		data_cpy = new char[strlen(data) + 1];
+		strcpy(data_cpy, data);
+	}
+
+	return new ASTNode { type, data_cpy, line, left, right, extra, nullptr };
 }
 
-ASTNode *append_node(ASTNode *head, ASTNode *new_node)
-{
+ASTNode* append_node(ASTNode* head, ASTNode* new_node) {
+	if (!head) {
+		return new_node;
+	}
+
+	ASTNode* curr = head;
+	while (curr->next) {
+		curr = curr->next;
+	}
+	curr->next = new_node;
+
+	return head;
 }
 
-void free_ast(ASTNode *node)
-{
-}
+void free_ast(ASTNode* node) {
+	if (!node) return;
 
+	free_ast(node->left);
+	free_ast(node->right);
+	free_ast(node->extra);
+	free_ast(node->next);
+
+	if (node->data) {
+		delete[] node->data;
+	}
+	delete node;
+}
 
 // DO NOT CHANGE THESE FUNCTIONS
 void print_indent(int level) {
-    for (int i = 0; i < level; i++) {
-        cout << "  ";
-    }
+	for (int i = 0; i < level; i++) {
+		cout << "  ";
+	}
 }
 
 void print_ast(ASTNode* node, int level) {
-    if (!node) return;
+	if (!node) return;
 
-    print_indent(level);
-    
-    switch (node->type) {
-        case NODE_PROGRAM:    cout << "Program" << endl; break;
-        case NODE_VAR_DECL:   cout << "VarDecl: " << (node->data ? node->data : "") << endl; break;
-        case NODE_FUNC_DECL:  cout << "FuncDecl: " << (node->data ? node->data : "") << endl; break;
-        case NODE_BLOCK:      cout << "Block" << endl; break;
-        case NODE_ASSIGN:     cout << "Assign" << endl; break;
-        case NODE_IF:         cout << "If" << endl; break;
-        case NODE_WHILE:      cout << "While" << endl; break;
-        case NODE_FOR:        cout << "For" << endl; break;
-        case NODE_RETURN:     cout << "Return" << endl; break;
-        case NODE_BIN_OP:     cout << "Op: " << (node->data ? node->data : "") << endl; break;
-        case NODE_UNARY_OP:   cout << "Unary: " << (node->data ? node->data : "") << endl; break;
-        case NODE_LITERAL:    cout << "Literal: " << (node->data ? node->data : "") << endl; break;
-        case NODE_VAR_USE:    cout << "Var: " << (node->data ? node->data : "") << endl; break;
-        case NODE_FUNC_CALL:  cout << "Call: " << (node->data ? node->data : "") << endl; break;
-        case NODE_IO:         cout << "IO: " << (node->data ? node->data : "") << endl; break;
-        default:              cout << "Unknown Node" << endl; break;
-    }
+	print_indent(level);
 
-    print_ast(node->left, level + 1);
-    print_ast(node->right, level + 1);
-    print_ast(node->extra, level + 1);
+	switch (node->type) {
+	case NODE_PROGRAM:
+		cout << "Program" << endl;
+		break;
+	case NODE_VAR_DECL:
+		cout << "VarDecl: " << (node->data ? node->data : "") << endl;
+		break;
+	case NODE_FUNC_DECL:
+		cout << "FuncDecl: " << (node->data ? node->data : "") << endl;
+		break;
+	case NODE_BLOCK:
+		cout << "Block" << endl;
+		break;
+	case NODE_ASSIGN:
+		cout << "Assign" << endl;
+		break;
+	case NODE_IF:
+		cout << "If" << endl;
+		break;
+	case NODE_WHILE:
+		cout << "While" << endl;
+		break;
+	case NODE_FOR:
+		cout << "For" << endl;
+		break;
+	case NODE_RETURN:
+		cout << "Return" << endl;
+		break;
+	case NODE_BIN_OP:
+		cout << "Op: " << (node->data ? node->data : "") << endl;
+		break;
+	case NODE_UNARY_OP:
+		cout << "Unary: " << (node->data ? node->data : "") << endl;
+		break;
+	case NODE_LITERAL:
+		cout << "Literal: " << (node->data ? node->data : "") << endl;
+		break;
+	case NODE_VAR_USE:
+		cout << "Var: " << (node->data ? node->data : "") << endl;
+		break;
+	case NODE_FUNC_CALL:
+		cout << "Call: " << (node->data ? node->data : "") << endl;
+		break;
+	case NODE_IO:
+		cout << "IO: " << (node->data ? node->data : "") << endl;
+		break;
+	default:
+		cout << "Unknown Node" << endl;
+		break;
+	}
 
-    print_ast(node->next, level);
+	print_ast(node->left, level + 1);
+	print_ast(node->right, level + 1);
+	print_ast(node->extra, level + 1);
+
+	print_ast(node->next, level);
 }
--- PA2/src/ast.h
+++ PA2/src/ast.h
@@ -2,41 +2,41 @@
 #define AST_H
 
 typedef enum NodeType {
-    NODE_PROGRAM,
-    NODE_VAR_DECL,
-    NODE_FUNC_DECL,
-    NODE_BLOCK,
-    NODE_ASSIGN,
-    NODE_IF,
-    NODE_WHILE,
-    NODE_FOR,
-    NODE_RETURN,
-    NODE_BIN_OP,    // +, -, *, /, etc.
-    NODE_UNARY_OP,  // x++, --x
-    NODE_LITERAL,   // 10, 3.14, true
-    NODE_VAR_USE,   // x
-    NODE_FUNC_CALL,
-    NODE_IO         // cin/cout
+	NODE_PROGRAM,
+	NODE_VAR_DECL,
+	NODE_FUNC_DECL,
+	NODE_BLOCK,
+	NODE_ASSIGN,
+	NODE_IF,
+	NODE_WHILE,
+	NODE_FOR,
+	NODE_RETURN,
+	NODE_BIN_OP,   // +, -, *, /, etc.
+	NODE_UNARY_OP, // x++, --x
+	NODE_LITERAL,  // 10, 3.14, true
+	NODE_VAR_USE,  // x
+	NODE_FUNC_CALL,
+	NODE_IO // cin/cout
 } NodeType;
 
 typedef struct ASTNode {
-    NodeType type;
-    char* data;             // Stores the operator ("+"), ID ("x"), or Value ("10")
-    int line;
-    struct ASTNode* left;   // Child 1 (e.g., LHS of assignment, or If-Condition)
-    struct ASTNode* right;  // Child 2 (e.g., RHS of assignment, or If-Body)
-    struct ASTNode* extra;  // Child 3 (e.g., Else-Body)
-    struct ASTNode* next;   // For lists (next statement in a block)
+	NodeType type;
+	char* data; // Stores the operator ("+"), ID ("x"), or Value ("10")
+	int line;
+	struct ASTNode* left;  // Child 1 (e.g., LHS of assignment, or If-Condition)
+	struct ASTNode* right; // Child 2 (e.g., RHS of assignment, or If-Body)
+	struct ASTNode* extra; // Child 3 (e.g., Else-Body)
+	struct ASTNode* next;  // For lists (next statement in a block)
 } ASTNode;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-    ASTNode* create_node(NodeType type, char* data, int line, ASTNode* left, ASTNode* right);
-    ASTNode* append_node(ASTNode* head, ASTNode* new_node);
-    void print_ast(ASTNode* node, int level);
-    void free_ast(ASTNode* node);
+ASTNode* create_node(NodeType type, char* data, int line, ASTNode* left, ASTNode* right);
+ASTNode* append_node(ASTNode* head, ASTNode* new_node);
+void print_ast(ASTNode* node, int level);
+void free_ast(ASTNode* node);
 
 #ifdef __cplusplus
 }
